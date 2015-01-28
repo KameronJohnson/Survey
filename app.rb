68463270f5b413bb6738a51f4f5226cda9a461d6
Kameron Johnson & James Maynard <student@epicodus.com>
2015-01-28 11:16:34 -0800

    Create survey post methods on app.rb

--- app.rb
+++ app.rb
@@ -2,3 +2,15 @@ require("bundler/setup")
 Bundler.require(:default, :development)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @surveys = Survey.all()
+  erb(:index)
+end
+
+post('/surveys') do
+  survey_name = params.fetch("survey_name")
+  @survey = Survey.create({ :survey_name => survey_name })
+  @surveys = Survey.all()
+  erb(:index)
+end
