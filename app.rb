25d98f47ab6c16bb0cf4221d238de5e85527a2e6
Kameron Johnson & James Maynard <student@epicodus.com>
2015-01-28 12:07:50 -0800

    add basic sinatra functionality with questions post

--- app.rb
+++ app.rb
@@ -14,3 +14,20 @@ post('/surveys') do
   @surveys = Survey.all()
   erb(:index)
 end
+
+post('/questions') do
+  question_var = params.fetch("the_question")
+  survey_id = params.fetch("survey_id")
+  @question = Question.create({ :the_question => question_var, :survey_id => survey_id })
+  redirect("/survey/#{survey_id}")
+end
+
+get('/survey/:id') do
+  @survey = Survey.find(params.fetch("id"))
+  erb(:surveys)
+end
+
+get('/questions') do
+  @questions = Question.all()
+  erb(:surveys)
+end
